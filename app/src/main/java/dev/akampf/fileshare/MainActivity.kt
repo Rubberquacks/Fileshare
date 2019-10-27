79521cc565b60bdcc8b98e68daf37ab763a349bc
Pirmin Braun <pirminbraun16@gmail.com>
2019-10-27 20:45:52 +0100

    Add button which gets Document from user and logs infos about it

--- app/src/main/java/dev/akampf/fileshare/MainActivity.kt
+++ app/src/main/java/dev/akampf/fileshare/MainActivity.kt
@@ -1,12 +1,120 @@
 package dev.akampf.fileshare
 
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
+
+private const val READ_REQUEST_CODE: Int = 42
+
+private const val LOGGING_TAG: String = "own_logs"
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOGGING_TAG, "Hello World!")
     }
+
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    private fun performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            // Filter to only show results that can be "opened", such as a
+            // file (as opposed to a list of contacts or timezones)
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+            // Filter to show only images, using the image MIME data type.
+            // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+            // To search for all documents available via installed storage providers,
+            // it would be "*/*".
+            type = "*/*"
+        }
+
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        Log.e(LOGGING_TAG, "button pressed")
+        val editText = findViewById<TextView>(R.id.textView)
+        editText.text = "blaaaa"
+        this.performFileSearch()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            resultData?.data?.also { uri ->
+                Log.i(LOGGING_TAG, "Uri: $uri")
+                this.dumpImageMetaData(uri)
+                //showImage(uri)
+            }
+        }
+    }
+
+    private fun dumpImageMetaData(uri: Uri) {
+
+        // The query, since it only applies to a single document, will only return
+        // one row. There's no need to filter, sort, or select fields, since we want
+        // all fields for one document.
+        val cursor: Cursor? = contentResolver.query(uri, null, null, null, null, null)
+
+        cursor?.use {
+            // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+            // "if there's anything to look at, look at it" conditionals.
+            if (it.moveToFirst()) {
+
+                // Note it's called "Display Name".  This is
+                // provider-specific, and might not necessarily be the file name.
+                val displayName: String =
+                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                Log.i(LOGGING_TAG, "Display Name: $displayName")
+
+                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                // If the size is unknown, the value stored is null.  But since an
+                // int can't be null in Java, the behavior is implementation-specific,
+                // which is just a fancy term for "unpredictable".  So as
+                // a rule, check if it's null before assigning to an int.  This will
+                // happen often:  The storage API allows for remote files, whose
+                // size might not be locally known.
+                val size: String = if (!it.isNull(sizeIndex)) {
+                    // Technically the column stores an int, but cursor.getString()
+                    // will do the conversion automatically.
+                    it.getString(sizeIndex)
+                } else {
+                    "Unknown"
+                }
+                Log.i(LOGGING_TAG, "Size: $size")
+            }
+        }
+    }
+
+
+
 }
